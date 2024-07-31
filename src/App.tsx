99b41b672d40fbdf339d6987d6c4f91af9c7de99
Netto <orlandiomnetto@gmail.com>
2024-07-31 07:22:49 -0300

    - Adicionado botão flutuante de adicionar matéria - Ajustes de código - Esboço da tela de adicionar matéria

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import MobileAddButton from "./components/MobileAddButton";
 import MobileHeader from "./components/MobileHeader";
 import MobileList from "./components/MobileList";
 import MobileSubHeader from "./components/MobileSubHeader";
@@ -9,7 +10,7 @@ import MobileSubHeader from "./components/MobileSubHeader";
 function App() {
   return (
     <>
-      <div className="h-screen flex flex-col bg-color2">
+      <div className="h-fit-content flex flex-col bg-color2">
         <Header />
         {/* <div className="sticky top-0 z-50"> */}
         <div className="sticky top-0 z-50 bg-color1">
@@ -17,6 +18,7 @@ function App() {
           <MobileSubHeader />
         </div>
         <MobileList />
+        <MobileAddButton />
         <Body />
         <Footer />
       </div>
---
+++ src/components/MobileAddButton.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { GlobalContext } from "../providers/GlobalContext";
+
+export default function MobileAddButton() {
+  const { setActiveModal } = React.useContext(GlobalContext);
+  return (
+    <button
+      className="sticky block sm:hidden bottom-6 ml-auto mr-6"
+      onClick={() => setActiveModal("add")}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="60"
+        height="60"
+        fill="currentColor"
+        className="bi bi-plus-circle-fill text-customOrange"
+        viewBox="0 0 16 16"
+      >
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+      </svg>
+    </button>
+  );
+}
