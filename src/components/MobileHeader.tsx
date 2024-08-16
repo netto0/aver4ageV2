4435e6a196c2581f67482852ceae7feb78cbcc81
Netto <orlandiomnetto@gmail.com>
2024-08-16 07:27:59 -0300

    - Alterado o layout do header mobile

--- src/components/MobileHeader.tsx
+++ src/components/MobileHeader.tsx
@@ -1,20 +1,22 @@
-import avatarImage from "../assets/avatar-default.svg";
+// import avatarImage from "../assets/avatar-default.svg";
+
 
 export default function MobileHeader() {
   return (
-    <header className="bg-[#01080e] flex sm:hidden justify-between align-middle text-textColor drop-shadow-sm py-4 px-6">
-      <div className="flex self-center gap-5">
+    <header className="bg-[#01080e] flex gap-6 sm:hidden justify-between align-middle text-textColor drop-shadow-sm py-4 px-6">
+      <div className="flex self-center gap-5 w-full items-center">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="25"
-          height="25"
+          width="22"
+          height="22"
           fill="currentColor"
-          className="bi bi-search"
+          className="bi bi-search absolute ml-4"
           viewBox="0 0 16 16"
         >
           <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
         </svg>
-        <svg
+        <input type="text" className="h-10 w-full bg-color2 rounded-md"/>
+        {/* <svg
           xmlns="http://www.w3.org/2000/svg"
           width="25"
           height="25"
@@ -23,11 +25,24 @@ export default function MobileHeader() {
           viewBox="0 0 16 16"
         >
           <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2z" />
-        </svg>
+        </svg> */}
       </div>
-      <div className="bg-textColor w-11 h-11 rounded-full">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="40"
+        height="40"
+        fill="currentColor"
+        className="bi bi-list"
+        viewBox="0 0 16 16"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
+        />
+      </svg>
+      {/* <div className="bg-textColor w-11 h-11 rounded-full">
         <img src={avatarImage} alt="avatar" className="scale-75" />
-      </div>
+      </div> */}
     </header>
   );
 }
