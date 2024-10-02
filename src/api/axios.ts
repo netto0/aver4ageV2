aaadd2461625974b14a67aa5dcab5679ddaa8c6f
Netto <orlandiomnetto@gmail.com>
2024-10-02 07:00:23 -0300

    - HTTPS url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,4 +1,4 @@
 // const baseUrl = "http://localhost:3000/average_api";
-const baseUrl = "http://averageapi.vercel.app/average_api";
+const baseUrl = "https://averageapi.vercel.app/average_api";
 
 export { baseUrl };
